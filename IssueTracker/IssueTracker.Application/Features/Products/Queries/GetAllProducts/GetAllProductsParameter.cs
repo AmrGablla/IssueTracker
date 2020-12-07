dd26ee8cfef496048fdeefc625a5a723df1b58c5
Amr GabAllah <amrjablla@gmail.com>
2020-12-07 23:05:49 +0200

    init

---
+++ IssueTracker/IssueTracker.Application/Features/Products/Queries/GetAllProducts/GetAllProductsParameter.cs
@@ -0,0 +1,12 @@
+﻿using IssueTracker.Application.Filters;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IssueTracker.Application.Features.Products.Queries.GetAllProducts
+{
+    public class GetAllProductsParameter : RequestParameter
+    {
+
+    }
+}
---
+++ IssueTracker/IssueTracker.Application/Mappings/GeneralProfile.cs
@@ -0,0 +1,20 @@
+﻿using IssueTracker.Application.Features.Products.Commands.CreateProduct;
+using IssueTracker.Application.Features.Products.Queries.GetAllProducts;
+using AutoMapper;
+using IssueTracker.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IssueTracker.Application.Mappings
+{
+    public class GeneralProfile : Profile
+    {
+        public GeneralProfile()
+        {
+            CreateMap<Product, GetAllProductsViewModel>().ReverseMap();
+            CreateMap<CreateProductCommand, Product>();
+            CreateMap<GetAllProductsQuery, GetAllProductsParameter>();
+        }
+    }
+}
